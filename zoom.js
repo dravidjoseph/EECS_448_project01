ee634474d46ccb00af87d93bc371dc3463f8a116
Francinaldo Almeida <fap_sb@globomail.com>
2016-03-03 20:25:15 -0600

    Add zoom feature

---
+++ zoom.js
@@ -0,0 +1,30 @@
+var minSize = 10;
+var maxSize = 100;
+
+$('#zoomIn').click(function(){    
+	fontSize= parseInt($('#time').css('font-size'));
+	newFontSize = fontSize + 10;
+
+	if(newFontSize <= maxSize){
+		$("#zoomOut").attr("disabled",false);
+		$('#time').css('font-size', newFontSize);
+	}
+
+	if(newFontSize==maxSize){
+		$("#zoomIn").attr("disabled",true);
+	}
+});
+
+$('#zoomOut').click(function(){
+	fontSize= parseInt($('#time').css('font-size'));
+	newFontSize = fontSize - 10;
+
+	if(newFontSize >= minSize){
+		$("#zoomIn").attr("disabled",false);
+		$('#time').css('font-size', newFontSize);
+	}
+	
+	if(newFontSize==minSize){
+		$("#zoomOut").attr("disabled",true);
+	}
+}); 
