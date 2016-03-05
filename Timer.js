dce56a8547dbefb0733ad662566b82c8c3a115fc
Jinuk Park <kebiya93@gmail.com>
2016-03-05 11:08:07 -0600

    Create Timer.js and add timerpanel

---
+++ Timer.js
@@ -0,0 +1,24 @@
+/**
+ * Timer.js
+ * @author Jinuk Park
+ */
+
+function timerSet() {
+
+}
+
+function timerStart() {
+
+}
+
+function timerPause() {
+
+}
+
+function timerResume() {
+
+}
+
+function timerReset() {
+
+}
