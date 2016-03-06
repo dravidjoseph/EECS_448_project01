182554951304cb0e19ebf63d9e9b926b5392fae3
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-06 08:59:42 -0600

    Add stopwatch string formatting

--- stopwatch.js
+++ stopwatch.js
@@ -31,3 +31,34 @@ var stopWatchObject = function(){
 	}
 	
 }
+
+
+var stopWatch = new stopWatchObject();
+var stopWatchString;
+var clockTimeInterval;
+
+function padStopWatchString(timeString, size){
+	var newTimeString = "0000" + num
+	return newTimeString.substr(newTimeString.length - size)
+}
+
+function formatTime(time){
+	var hours = 0
+	var minutes = 0
+	var seconds = 0
+	var ms = 0
+	var newTime = ''
+	
+	hours = Math.floor(time/(60*60*1000))
+	time = time % (60*60*1000)
+	
+	minutes = Math.floor(time/(60*1000))
+	time = time % (60*1000)
+	
+	seconds = Math.floor(time/(1000))
+	ms = time % 1000
+	
+	newTime = padStopWatchString(hours,2) + ':' + padStopWatchString(minutes,2) + ":" + padStopWatchString(seconds,2) + ":"+ padStopWatchString(ms,3)
+	return newTime;
+	
+}
