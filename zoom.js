c89ab841b8adaaaa600ae732464994b175a01495
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-06 10:59:35 -0600

    Add zoom functionality to stopwatch

--- zoom.js
+++ zoom.js
@@ -27,4 +27,32 @@ $('#zoomOut').click(function(){
 	if(newFontSize==minSize){
 		$("#zoomOut").attr("disabled",true);
 	}
-}); 
+});
+
+$('#zoomInSW').click(function(){    
+	fontSize= parseInt($('#stop').css('font-size'));
+	newFontSize = fontSize + 10;
+
+	if(newFontSize <= maxSize){
+		$("#zoomOutSW").attr("disabled",false);
+		$('#stop').css('font-size', newFontSize);
+	}
+
+	if(newFontSize==maxSize){
+		$("#zoomInSW").attr("disabled",true);
+	}
+});
+
+$('#zoomOutSW').click(function(){
+	fontSize= parseInt($('#stop').css('font-size'));
+	newFontSize = fontSize - 10;
+
+	if(newFontSize >= minSize){
+		$("#zoomInSW").attr("disabled",false);
+		$('#stop').css('font-size', newFontSize);
+	}
+
+	if(newFontSize==minSize){
+		$("#zoomOutSW").attr("disabled",true);
+	}
+});  
